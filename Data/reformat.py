e7a642fad19399d1f8f5c72c307b630f106fbb8f
Trey Nohren <twnohren@ucdavis.edu>
2017-03-18 20:51:09 -0700

    Hopefully correctly formatted JSON this time

--- Data/reformat.py
+++ Data/reformat.py
@@ -20,35 +20,39 @@ jsonFile = open("all.json", "w")
 fileList = [coffeeFile, dairyFile, fishFile, fruitFile, grainFile, meatFile, nutFile, otherFile, sweetFile, vegetableFile, vegoilFile]
 fileNameList = ["coffeetea", "dairy", "fish", "fruits", "grains", "meats", "nuts", "other", "sweets", "vegetables", "vegoils"]
 
-jsonVar = {"countries": {}}
+jsonVar = {"name": "top", "children": []}
 
 totFile.readline()
 for line in totFile:
     temp = line.split(",")
     if (temp[0] == "Rest of world" or temp[0] == "Total"): break
-    jsonVar["countries"][temp[0]] = {}
+    jsonVar["children"].append({"name": temp[0], "children": []})
 
 for i in range(1999, 2015):
-    for country in jsonVar["countries"]:
-        jsonVar["countries"][country][str(i)] = {}
+    for country in jsonVar["children"]:
+        temp = []
         for nm in fileNameList:
-            jsonVar["countries"][country][str(i)][nm] = {}
+            temp.append({"name": nm, "children":[]})
+        country["children"].append({"children":temp, "name": str(i)})
 
 for i in range(0, len(fileList)):
     header = fileList[i].readline()
     header = re.split(', |\n', header)
     header = header[0].split(",")
-    
+
+    countryCount = 0
     for line in fileList[i]:
         temp = line.split(",")
         curCountry = temp[0]
         index = 2
-        while ((2014 - 1999 + index) < len(temp) and (2014 - 1999 + index) < len(header)) :
+        while ((2014 - 1999 + index) < len(temp) and (2014 - 1999 + index) < len(header)):
             if (curCountry != "Rest of world" and curCountry != "Total"):
                 if (temp[index] != ""):
                     for j in range(1999, 2015):
-                        jsonVar["countries"][curCountry][str(j)][fileList[i].name[:-4]][header[j - 1999 + index][5:len(header[j - 1999 + index])]] = temp[j - 1999 + index]
+                        jsonVar["children"][countryCount]["children"][j - 1999]["children"][i]["children"].append({"name":header[j - 1999 + index][5:len(header[j - 1999 + index])],"value": temp[j - 1999 + index]})
             index += 16
+        countryCount += 1
+    countryCount = 0
 
 json.dump(jsonVar, jsonFile)
 jsonFile.close()

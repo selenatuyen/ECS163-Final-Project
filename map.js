f1c5260ddff59544591833fee0b0960760a5aff0
Selena Tuyen <sltuyen@ucdavis.edu>
2017-03-20 14:34:54 -0700

    Change country color fill

--- map.js
+++ map.js
@@ -45,11 +45,11 @@ var tip = d3.select("body")
 // };
 
 var arccolor = {
-    "AF": "#81F495",
+    "AF": "#B72828",
     "AS": "#63ADF2",
     "EU": "#EDC16C",
-    "NA": "#A447B2",
-    "OC": "#F44985",
+    "NA": "#F433EB",
+    "OC": "#9733F4",
     "SA": "#F9965C",
     "ALL": "#8D8187"
 };
@@ -103,11 +103,11 @@ function drawMap() {
         })
         .attr("fill", function(d) { 
             if (parseInt(d.id, 10) in importAmount && (importAmount[parseInt(d.id, 10)].Continent == selectedContinent || selectedContinent == "ALL")) {
-                return "#FFFFFF"
+                return "#A7C1A6"
             } else if (d.id == 840) {
-                return "#880000";
+                return "#7FAD7C";
             } else {
-                return "#A9A9A9";
+                return "#546B84";
             }
         })
         .on("mouseover", function(d) {
